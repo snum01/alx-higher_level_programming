217ba67a75300cb8c3096a156f58f4e9434883a9
snum01 <johnjim4life@gmail.com>
2023-01-11 12:54:17 +0000

    task 100

---
+++ 0x0B-python-input_output/tests/100-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+append_after = __import__('100-append_after').append_after
+
+append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact
