fecd0bebc798c964117c8d71e645bb89328ddf73
snum01 <johnjim4life@gmail.com>
2023-03-13 15:28:46 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints 3 lines using an array of string and a loop
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing']; let i;
+for (i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
