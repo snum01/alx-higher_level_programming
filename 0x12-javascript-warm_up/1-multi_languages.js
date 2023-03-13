15c9306c0188ccb247c29c1040fbf6ab99bcaf39
snum01 <johnjim4life@gmail.com>
2023-03-13 15:09:44 -0700

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that prints 3 lines about C, Python and JS
+
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'Javascript is amazing';
+
+console.log(myVar1 + '\n' + myVar2 + '\n' + myVar3);
