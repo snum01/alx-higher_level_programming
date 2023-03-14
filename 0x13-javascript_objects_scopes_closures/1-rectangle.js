3575cc25dbb958c23c7d5de7776385d176e95a41
snum01 <johnjim4life@gmail.com>
2023-03-14 10:31:36 -0700

    task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// A class Rectangle with a constructor must take 2 arguments w and h
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
